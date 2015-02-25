d2b07114a4319a1cb71873f5486ed10b580db379
Wamm K. D <jaft@brandeis.edu>
2015-02-25 11:29:02 -0500

    Fixed authentication

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,4 +1,8 @@
 class UploadController < ApplicationController
   def index
+    if(session[:user_id] == nil)
+      flash[:notice] = "You must be logged in to view that page!"
+      redirect_to log_in_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
-	def index
- 	end
- 	
-	def new
-		@user = User.new
-	end
+  def index
+  end
+  
+  def new
+    @user = User.new
+  end
 
-	def create 
-		@user = User.new(user_params)
-		if @user.save
-			flash[:notice] = "Welcome to the App!"
-			redirect_to "/"
-		else
-			flash[:alert] = "Problem. Please Try again."
-			redirect_to "back"
-		end
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the App!"
+      redirect_to "/"
+    else
+      flash[:alert] = "Problem. Please Try again."
+      redirect_to "back"
+    end
 
-	end
+  end
 
-	private 
-	def user_params
-		params.require(:user).permit(:email, :password, :password_confirmation)
-	end
+  private 
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
