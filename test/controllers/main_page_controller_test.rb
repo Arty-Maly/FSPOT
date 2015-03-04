ec8e178eaecd9dfb65371127760f7dd10f38c57d
Arty-Maly <artem@brandeis.edu>
2015-03-04 15:51:47 -0500

    comments

--- test/controllers/main_page_controller_test.rb
+++ test/controllers/main_page_controller_test.rb
@@ -1,10 +1,14 @@
 require 'test_helper'
 
 class MainPageControllerTest < ActionController::TestCase
+  
+	#if logged in should be able to get to closed parts
   test "should get access" do
     get :index
     assert_response :success
   end
+
+  #if not logged in should not be able to get to closed parts
 
   test "should not get access" do
     logout
