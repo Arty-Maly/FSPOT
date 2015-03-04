0fc28c8eeacd5d1297a126b765cadabd1459d6fc
Arty-Maly <artem@brandeis.edu>
2015-03-04 15:36:45 -0500

    Added login tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,13 +1,20 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should not get index" do
+  
+  test "should not redirect when logged in" do
     get :new
-    assert_response :redirect
+    assert_response :success
+  end
+  #test if the user can login
+  test "should login" do
+  	bob = users(:one)
+  	post :create, :email => bob.email, :password => 'secret'
+  	assert_equal bob.id, session[:user_id]
   end
 
-  test "should get index" do
-    logout
+  test "should not redirect when logged out" do
+	logout
     get :new
     assert_response :success
   end
