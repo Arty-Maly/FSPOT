618e78609e067ecaecd9c375432c6e45ba9126e5
Arty-Maly <artem@brandeis.edu>
2015-03-04 15:49:48 -0500

    more tests

--- test/controllers/main_page_controller_test.rb
+++ test/controllers/main_page_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class MainPageControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should get access" do
     get :index
     assert_response :success
   end
 
-  test "should not get index" do
+  test "should not get access" do
     logout
     get :index
     assert_response :redirect
