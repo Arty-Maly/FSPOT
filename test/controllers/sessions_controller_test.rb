41ef086e7335038d9b0bf460294b65088215a79e
Wamm K. D <jaft@brandeis.edu>
2015-03-06 12:28:04 -0500

    Adding secrets.yml file that should allow us to view from heroku in browser

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  
   test "should not redirect when logged in" do
     get :new
     assert_response :success
   end
+
   #test if the user can login
   test "should login" do
-  	bob = users(:one)
-  	post :create, :email => bob.email, :password => 'secret'
-  	assert_equal bob.id, session[:user_id]
+    bob = users(:one)
+    post :create, :email => bob.email, :password => 'secret'
+    assert_equal bob.id, session[:user_id]
   end
 
   test "should not redirect when logged out" do
-	logout
+    logout
     get :new
     assert_response :success
   end
