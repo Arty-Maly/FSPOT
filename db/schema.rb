64f501f82f81c82117f8fdf7778141303980d55d
Arty-Maly <artem@brandeis.edu>
2015-03-18 16:08:33 -0400

    Added Relationship models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305194451) do
+ActiveRecord::Schema.define(version: 20150318191327) do
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "uploads", force: true do |t|
     t.string   "name"
