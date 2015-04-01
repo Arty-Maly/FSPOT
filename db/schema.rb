a9a55eba7666f57864a82df8c1ae23d7527643ba
Arty-Maly <artem@brandeis.edu>
2015-04-01 14:59:50 -0400

    added rating column in uploads

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331195110) do
+ActiveRecord::Schema.define(version: 20150401185910) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150331195110) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "location"
+    t.integer  "rating"
   end
 
   add_index "uploads", ["user_id"], name: "index_uploads_on_user_id"
