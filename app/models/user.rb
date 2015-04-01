1408dffa320b8541e6841f879867b34a0f2cc117
Huilin Gang <ganghuilin.tju@gmail.com>
2015-04-01 12:01:54 -0400

    Added Destroy to comments in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   
   attr_accessor :password
   validates_confirmation_of :password
