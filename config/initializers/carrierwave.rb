b7821a6b4f57cbbb1f5cd9117d5f9cbb6278c397
Arty-Maly <artem@brandeis.edu>
2015-04-01 13:56:15 -0400

    Added local storage for development

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-                            :provider              => 'AWS',
-                            :aws_access_key_id     => ENV["AWS_ACCESS_KEY"],
-                            :aws_secret_access_key => ENV["AWS_SECRET_KEY"]
-                           }
-  config.fog_directory   = ENV["AWS_BUCKET"]
+  if(Rails.env.production? || Rails.env.test?)
+  	config.storage = :fog
+	config.fog_credentials = {
+    	                      :provider              => 'AWS',
+                           	  :aws_access_key_id     => ENV["AWS_ACCESS_KEY"],
+                              :aws_secret_access_key => ENV["AWS_SECRET_KEY"]
+                           	 }
+  	config.fog_directory   = ENV["AWS_BUCKET"]
+  else
+  	config.storage = :file
+  end
 end
