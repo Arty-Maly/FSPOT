5153f4d9556dd969040dc1af6a36598ffae38494
Arty-Maly <artem@brandeis.edu>
2015-04-15 17:18:48 -0400

    fixed some db issues

--- app/controllers/personal_controller.rb
+++ app/controllers/personal_controller.rb
@@ -1,14 +1,5 @@
 class PersonalController < ApplicationController
 	def index
-		puts params
-		puts "====================="
-		puts "====================="
-		puts "====================="
-		puts "====================="
-		puts "====================="
-		puts "====================="
-		puts "====================="
-		puts "====================="
-		puts "====================="
+		
 	end
 end
