a947bd08488bf1dfe4f4a3fa1959458e76a42548
Wamm K. D <jaft@brandeis.edu>
2015-04-28 13:32:16 -0400

    Added more elements to the config.assets.precompile list in config/environments/production.rb; Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,11 @@ Mvp::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += ['style.css']
+  config.assets.precompile += ['style.css',
+                               'slick.css',
+                               'slick.js',
+                               'mmenu.js',
+                               'mmenu.css']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
