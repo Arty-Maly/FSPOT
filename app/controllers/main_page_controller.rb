b6da1051f723f497ff56ed2a877f24bb81a588de
Arty-Maly <artem@brandeis.edu>
2015-05-05 15:38:42 -0400

    fixing layouts

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -2,7 +2,7 @@ class MainPageController < ApplicationController
 
   def index
     if(session[:user_id] == nil)
-      flash[:notice] = "You must be logged in to view that page!"
+      
       redirect_to root_path
     else
       if(params[:search] or params[:category])
